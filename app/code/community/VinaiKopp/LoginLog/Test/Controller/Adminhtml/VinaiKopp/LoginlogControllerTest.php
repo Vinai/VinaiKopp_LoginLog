6bb9dbed29408758f2a2e687716e321a3a814032
Vinai Kopp <vinai@netzarbeiter.com>
2014-03-21 18:53:37 +0100

    Implemented massAction, export XML and CSV actions

--- app/code/community/VinaiKopp/LoginLog/Test/Controller/Adminhtml/VinaiKopp/LoginlogControllerTest.php
+++ app/code/community/VinaiKopp/LoginLog/Test/Controller/Adminhtml/VinaiKopp/LoginlogControllerTest.php
@@ -81,4 +81,91 @@ class VinaiKopp_LoginLog_Test_Controller_Adminhtml_VinaiKopp_LoginlogControllerT
         $this->assertLayoutBlockCreated('vinaikopp.loginlog.grid');
         $this->assertLayoutBlockInstanceOf('vinaikopp.loginlog.grid', 'VinaiKopp_LoginLog_Block_Adminhtml_LoginLog_List_Grid');
     }
+    
+    public function testDeleteMassAction()
+    {
+        $this->app()->getRequest()->setPost('logins', array(1, 2, 3));
+        $mockLogin = $this->getModelMock('vinaikopp_loginlog/login');
+        $mockLogin->expects($this->exactly(3))
+            ->method('delete');
+        $mockLogin->expects($this->exactly(3))
+            ->method('setId')
+            ->will($this->returnSelf());
+        $this->replaceByMock('model', 'vinaikopp_loginlog/login', $mockLogin);
+        $this->dispatch('adminhtml/loginlog/deleteMass');
+    }
+    
+    public function testExportCsvActionExists()
+    {
+        $this->assertTrue(
+            is_callable(array($this->class, 'exportCsvAction')),
+            "Method exportCsvAction does not exist on {$this->class}"
+        );
+    }
+
+    /**
+     * @depends testExportCsvActionExists
+     * @throws Exception
+     * @throws Zend_Controller_Response_Exception
+     */
+    public function testExportCsvAction()
+    {
+        if (! function_exists('set_exit_overload')) {
+            $this->markTestSkipped("PHP-test-helper extension with exit overload not available, skipping test.");
+        }
+
+        $this->assertTrue($this->app()->getResponse()->canSendHeaders());
+        
+        set_exit_overload(function() {});
+        try {
+            $this->dispatch('adminhtml/loginlog/exportCsv');
+        } catch (Zend_Controller_Response_Exception $e) {
+            // Catch only headers already sent exception here
+            if ($e->getMessage() != 'Cannot send headers; headers already sent') {
+                throw $e;
+            }
+        }
+        unset_exit_overload();
+        
+        $this->assertTrue(false !== $this->app()->getLayout()->getBlock('vinaikopp_loginlog.export'));
+        $this->assertResponseHeaderContains('Content-Disposition', 'attachment; filename=');
+        $this->assertResponseHeaderEquals('Content-Type', 'application/octet-stream');
+    }
+    
+    public function testExportXmlActionExists()
+    {
+        $this->assertTrue(
+            is_callable(array($this->class, 'exportXmlAction')),
+            "Method exportXmlAction does not exist on {$this->class}"
+        );
+    }
+
+    /**
+     * @depends testExportXmlActionExists
+     * @throws Exception
+     * @throws Zend_Controller_Response_Exception
+     */
+    public function testExportXmlAction()
+    {
+        if (! function_exists('set_exit_overload')) {
+            $this->markTestSkipped("PHP-test-helper extension with exit overload not available, skipping test.");
+        }
+        
+        $this->assertTrue($this->app()->getResponse()->canSendHeaders());
+        
+        set_exit_overload(function() {});
+        try {
+            $this->dispatch('adminhtml/loginlog/exportXml');
+        } catch (Zend_Controller_Response_Exception $e) {
+            // Catch only headers already sent exception here
+            if ($e->getMessage() != 'Cannot send headers; headers already sent') {
+                throw $e;
+            }
+        }
+        unset_exit_overload();
+        
+        $this->assertTrue(false !== $this->app()->getLayout()->getBlock('vinaikopp_loginlog.export'));
+        $this->assertResponseHeaderContains('Content-Disposition', 'attachment; filename=');
+        $this->assertResponseHeaderEquals('Content-Type', 'application/octet-stream');
+    }
 } 
--- app/code/community/VinaiKopp/LoginLog/controllers/Adminhtml/LoginlogController.php
+++ app/code/community/VinaiKopp/LoginLog/controllers/Adminhtml/LoginlogController.php
@@ -24,4 +24,53 @@ class VinaiKopp_LoginLog_Adminhtml_LoginlogController
         $this->loadLayout(false);
         $this->renderLayout();
     }
+
+    public function deleteMassAction()
+    {
+        $data = $this->getRequest()->getPost('logins');
+        if ($data) {
+            try {
+                $num = 0;
+                $model = Mage::getModel('vinaikopp_loginlog/login');
+                foreach ((array)$data as $id) {
+                    $model->setId($id)->delete();
+                    $num++;
+                }
+                $this->_getSession()->addSuccess(
+                    $this->__('Deleted %s login log record(s)', $num)
+                );
+            } catch (Exception $e) {
+                $this->_getSession()->addError($e->getMessage());
+                Mage::logException($e);
+            }
+        }
+
+        $this->_redirect('*/*/index');
+    }
+
+    /**
+     * Export grid to CSV format
+     */
+    public function exportCsvAction()
+    {
+        $fileName   = 'logins.csv';
+        $content    = $this->getLayout()
+            ->createBlock('vinaikopp_loginlog/adminhtml_loginLog_list_grid', 'vinaikopp_loginlog.export')
+            ->getCsvFile();
+
+        $this->_prepareDownloadResponse($fileName, $content);
+    }
+
+    /**
+     * Export grid to XML format
+     */
+    public function exportXmlAction()
+    {
+        $fileName   = 'logins.xml';
+        $content    = $this->getLayout()
+            ->createBlock('vinaikopp_loginlog/adminhtml_loginLog_list_grid', 'vinaikopp_loginlog.export')
+            ->getExcelFile();
+
+        $this->_prepareDownloadResponse($fileName, $content);
+    }
 } 
